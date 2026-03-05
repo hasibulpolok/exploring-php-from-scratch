b3452cd5c1077c64b079055286d0c0dc75bc4cfe
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-05 10:15:20 +0600

    start exploring php from scratch

---
+++ home-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<?php
+$date ="05-March-2-26"
+?>
+<h1>Today is <?=$date;?></h1>
+</body>
+</html>
