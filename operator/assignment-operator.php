cf712d88c97c5972dc464e27b66927105ace1364
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-07 11:30:31 +0600

    assignment operator added

---
+++ operator/assignment-operator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment Operator</title>
+</head>
+<body>
+    <?php 
+    $x =5;
+    $x = $x+6;
+    echo $x;
+    
+    ?>
+    
+</body>
+</html>
