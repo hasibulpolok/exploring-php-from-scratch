48b09974a537421ecff089e7a9af7ee8504c3e41
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-09 11:48:02 +0600

    superglovalvariables

---
+++ form/superglobalvariable.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>New Form</title>
+</head>
+
+<body>
+
+<form action="" method="get">
+<label for="name">Name</label>
+<input type="text" name="name" id="name">
+<input type="submit" value="Submit">
+
+</form>
+
+<?php
+
+if(isset($_REQUEST['name'])){
+    $store = $_REQUEST['name'];
+    echo "<br>";
+    echo "User Name : ";
+    echo $store;
+}
+
+?>
+
+</body>
+</html>
