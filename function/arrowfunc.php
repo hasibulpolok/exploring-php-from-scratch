ad5a092e24164afb7d5556a14256cf3df7b03cf7
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 10:48:18 +0600

    comparison function

--- function/arrowfunc.php
+++ function/arrowfunc.php
@@ -2,4 +2,24 @@
 $add = fn($a, $b) => $a + $b;
 
 echo "The sum of two parameter is : " . $add(5,3);
+
+echo "<br>";
+$check = fn($a,$b) => $a > $b;
+
+var_dump($check(10,5));
+
+
+
+echo "<br>";
+
+$compare = function($a,$b){
+    if($a > $b){
+        echo "A is greater";
+    }else{
+        echo "B is greater";
+    }
+};
+
+$compare(2,5);
+
 ?>
