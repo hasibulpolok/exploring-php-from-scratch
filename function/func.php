3f69bd8fffc292ef0ab7d45003d1d94e51f78d5f
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 10:22:57 +0600

    func

--- function/func.php
+++ function/func.php
@@ -5,4 +5,16 @@ function sayHello(){
 
 sayHello();
 
+echo "<br>";
+
+function familyName($fname) {
+  echo "$fname Refsnes.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
 ?>
+
