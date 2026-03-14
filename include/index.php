fed9b59ffd4aef9802f20894fda87fddc2971c43
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-14 11:40:46 +0600

    index code fixed

--- include/index.php
+++ include/index.php
@@ -1,32 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
 </head>
+
 <body>
-<header>
-    <h1>আমার ওয়েবসাইটে স্বাগতম</h1>
-    <nav>
-        <a href="index.php">Home</a> |
-        <a href="#">About</a> |
-        <a href="#">Contact</a>
-    </nav>
-    <hr>
+    <header>
 
-    <?php
-// header include
-include 'header.php';
-?>
+        <?php
+        // header include
+        include 'header.php';
+        ?>
+    </header>
+     <main>
+            <h2>মেইন কন্টেন্ট</h2>
+            <p>এইখানে আপনার ওয়েবসাইটের প্রধান তথ্য থাকবে।</p>
+        </main>
 
-<main>
-    <h2>মেইন কন্টেন্ট</h2>
-    <p>এইখানে আপনার ওয়েবসাইটের প্রধান তথ্য থাকবে।</p>
-</main>
-
-<?php
-// footer include
-include 'footer.php';
-?>
-</header>
+        <?php
+        // footer include
+        include 'footer.php';
+        ?>
+</body>
+</html>
