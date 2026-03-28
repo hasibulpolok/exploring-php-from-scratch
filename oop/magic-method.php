0f66595fbb80a7d6454be58c1a92225ff098d3ee
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-28 11:35:29 +0600

    destruct() added

--- oop/magic-method.php
+++ oop/magic-method.php
@@ -5,7 +5,10 @@ class car
     public $name;
     public $color;
 
-   
+    public function __destruct()
+    {
+        echo "This is a destructor method<br>";
+    }
     public function __construct()
     {
         echo "This is a constructor method<br>";
