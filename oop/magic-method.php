5cb9a7f080bca12ca7473f31c0ec6f97a027d675
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-28 11:34:49 +0600

    constructor added in magic method

--- oop/magic-method.php
+++ oop/magic-method.php
@@ -1,2 +1,15 @@
 <?php
 
+class car
+{
+    public $name;
+    public $color;
+
+   
+    public function __construct()
+    {
+        echo "This is a constructor method<br>";
+    }
+}
+
+$result =new car();
