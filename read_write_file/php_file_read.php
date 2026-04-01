065a5815dc23841f92f0055882c7223222d36656
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-01 10:54:24 +0600

    die added

--- read_write_file/php_file_read.php
+++ read_write_file/php_file_read.php
@@ -1,7 +1,7 @@
 <?php
 
 // file rading 
-$result = fopen('first.txt', 'r');
+$result = fopen('first.txt', 'r') or die('Unable to open file!');
 
 echo fread($result,filesize('first.txt'));
 fclose($result);
