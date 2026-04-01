f95a62f382f4c992b6943afaaa114c5180f62e8b
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-01 11:42:14 +0600

    readfile txt updated

--- read_write_file/php_file_read.php
+++ read_write_file/php_file_read.php
@@ -6,7 +6,7 @@ $result = fopen('first.txt', 'r') or die('File not found!');
 echo fread($result,filesize('first.txt'));
 fclose($result);
 echo "<br><br>";
-echo readfile("data.txt");
+echo readfile("data.txt") or die("File not found!");
 
  $file_get = file_get_contents("fileget.txt") or die("File not found!");
 echo $file_get;
