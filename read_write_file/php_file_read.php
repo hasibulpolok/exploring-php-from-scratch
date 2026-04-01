8b7874d912566774c05ca426419ad64a9c15d026
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-01 10:56:34 +0600

    die code updated

--- read_write_file/php_file_read.php
+++ read_write_file/php_file_read.php
@@ -1,7 +1,7 @@
 <?php
 
 // file rading 
-$result = fopen('first.txt', 'r') or die('Unable to open file!');
+$result = fopen('first.txt', 'r') or die('File not found!');
 
 echo fread($result,filesize('first.txt'));
 fclose($result);
