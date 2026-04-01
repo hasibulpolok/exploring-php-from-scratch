4736faad6796376a7d25fea35aa019d25028f5c3
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-01 11:44:56 +0600

    break

--- read_write_file/php_file_read.php
+++ read_write_file/php_file_read.php
@@ -8,6 +8,8 @@ fclose($result);
 echo "<br><br>";
 echo readfile("data.txt") or die("File not found!");
 
+echo "<br><br>";
+
  $file_get = file_get_contents("fileget.txt") or die("File not found!");
 echo $file_get;
 ?>
