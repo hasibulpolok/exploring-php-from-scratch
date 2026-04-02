ecca74e617a78f06b2839ca3104393167404c5f8
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-02 12:00:15 +0600

    saved data on txt file

--- form-using-oop/index.php
+++ form-using-oop/index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <title>Document</title>
+    <title>Store Data</title>
 </head>
 <body>
-<form action="#" method="post">
-Id: <br>
-<input type="text" name="id"><br><br>
-Name: <br>
-<input type="text" name="name"><br><br>
+    <h2>Enter Data</h2>
+    <form method="post">
+        <label for="id">ID:</label>
+        <input type="text" name="id" id="id" required><br><br>
 
-</form>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required><br><br>
 
-   <?php 
+        <input type="submit" value="Save">
+    </form>
+
+    <?php
+
+    if (!empty($_POST['id']) && !empty($_POST['name'])) {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+
+     
+        $data = "ID: $id, Name: $name\n";
+
+       
+        file_put_contents("data.txt", $data, FILE_APPEND);
+
+        echo "<p>Data saved successfully!</p>";
+    }
+
    
-   
-   
-   ?> 
+    if (file_exists("data.txt")) {
+        echo "<h3>All Entries:</h3><pre>" . file_get_contents("data.txt") . "</pre>";
+    }
+    ?>
 </body>
 </html>
