06ac2ab2e78d7ff98c2fe542b417227ab4f6c617
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-02 11:38:59 +0600

    from using oop

---
+++ form-using-oop/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="#" method="post">
+Id: <br>
+<input type="text" name="id"><br><br>
+Name: <br>
+<input type="text" name="name"><br><br>
+
+</form>
+
+   <?php 
+   
+   
+   
+   ?> 
+</body>
+</html>
