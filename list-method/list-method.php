b89e2ffea6e4a9535f122445012a940947b88b02
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-02 10:02:39 +0600

    data list and loop style chnaged

--- list-method/list-method.php
+++ list-method/list-method.php
@@ -5,7 +5,12 @@ $person = [
     [2, 'Saurav Hossain', 26, 'saurav@gmail.com', '01823456789', 'Gazipur'],
     [3, 'Mustafijur Rahman', 25, 'mustafijur@gmail.com', '01934567890', 'Khulna'],
     [4, 'Tanvir Ahmed', 28, 'tanvir@gmail.com', '01645678901', 'Chittagong'],
-    [5, 'Nusrat Jahan', 24, 'nusrat@gmail.com', '01556789012', 'Rajshahi']
+    [5, 'Nusrat Jahan', 24, 'nusrat@gmail.com', '01556789012', 'Rajshahi'],
+    [6, 'Rakib Hasan', 29, 'rakib@gmail.com', '01367890123', 'Sylhet'],
+    [7, 'Farhana Akter', 23, 'farhana@gmail.com', '01778901234', 'Barisal'],
+    [8, 'Imran Khan', 30, 'imran@gmail.com', '01889012345', 'Comilla'],
+    [9, 'Jannat Ara', 22, 'jannat@gmail.com', '01990123456', 'Rangpur'],
+    [10, 'Shakib Al Hasan', 35, 'shakib@gmail.com', '01601234567', 'Magura']
 ];
 
 foreach ($person as list($id, $name, $age, $email, $phone, $city)) {
@@ -16,4 +21,5 @@ foreach ($person as list($id, $name, $age, $email, $phone, $city)) {
     echo "Phone: $phone <br>";
     echo "City: $city <br><br>";
 }
+
 ?>
