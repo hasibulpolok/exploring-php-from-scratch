1c606bbb07f1bd42b4b7f0558544fe03fa2b81a5
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-05 09:31:47 +0600

    storing data

--- form-using-oop/form.php
+++ form-using-oop/form.php
@@ -9,10 +9,10 @@
 
 <form method="POST">
     
-    ID: <input type="text" name="id"><br><br>
-    Name: <input type="text" name="name"><br><br>
-    Address: <input type="text" name="address"><br><br>
-    Contact: <input type="text" name="contact"><br><br>
+    ID: <input type="text" name="id" required><br><br>
+    Name: <input type="text" name="name" required><br><br>
+    Address: <input type="text" name="address" required><br><br>
+    Contact: <input type="text" name="contact" required><br><br>
 
     <input type="submit" name="submit" value="Submit">
 </form>
@@ -25,15 +25,13 @@ class User {
     public $address;
     public $contact;
 
-
     public function __construct($name, $id, $address, $contact) {
         $this->id = $id;
-         $this->name = $name;
+        $this->name = $name;
         $this->address = $address;
         $this->contact = $contact;
     }
 
-  
     public function display() {
         echo "<h3>Submitted Data:</h3>";
         echo "Name: " . $this->name . "<br>";
@@ -41,16 +39,33 @@ class User {
         echo "Address: " . $this->address . "<br>";
         echo "Contact: " . $this->contact . "<br>";
     }
+
+
+    public function saveToFile() {
+        $data = "Name: " . $this->name . "\n" .
+                "ID: " . $this->id . "\n" .
+                "Address: " . $this->address . "\n" .
+                "Contact: " . $this->contact . "\n";
+        $data .= "--------------------------\n";
+
+        file_put_contents("users.txt", $data, FILE_APPEND);
+    }
 }
 
-
 if (isset($_POST['submit'])) {
-    $user = new User(
-        $_POST['name'],
-        $_POST['id'],
-        $_POST['address'],
-        $_POST['contact']
-    );
+
+
+    $name = htmlspecialchars($_POST['name']);
+    $id = htmlspecialchars($_POST['id']);
+    $address = htmlspecialchars($_POST['address']);
+    $contact = htmlspecialchars($_POST['contact']);
+
+
+    $user = new User($name, $id, $address, $contact);
+
+
+    $user->saveToFile();
+
 
     $user->display();
 }
