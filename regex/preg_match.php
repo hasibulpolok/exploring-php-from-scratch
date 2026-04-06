eee0db37e7d7ca61a9174023f7b536c40a7e2017
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-06 10:50:00 +0600

    user txt

--- regex/preg_match.php
+++ regex/preg_match.php
@@ -6,4 +6,19 @@ if(preg_match("/Polok/", $text)){
 }else{
     echo "Not found";
 }
+
+$text = "PHP is easy. PHP is powerful.";
+
+preg_match_all("/PHP/", $text, $result);
+
+print_r($result[0]);
+
+$email = "test@gmail.com";
+
+if(preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/", $email)){
+    echo "Valid Email";
+}else{
+    echo "Invalid Email";
+}
+
 ?>
