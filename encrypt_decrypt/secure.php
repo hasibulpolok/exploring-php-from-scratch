c0f1242089f601d0da99c08402a96b6c51d6db82
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-04-13 10:56:42 +0600

    code reversed

--- encrypt_decrypt/secure.php
+++ encrypt_decrypt/secure.php
@@ -1,24 +1,22 @@
 <?php
 $username = "admin";
-$password = "1234";
+$password="1234";
 
-echo "<h2 style='color:blue;'>🔐 Hash Generator Result</h2>";
+echo md5($username) . "\n";
+echo md5($password);
 
-// MD5 Section
-echo "<h3 style='color:green;'>MD5 Hash</h3>";
-echo "Username: " . md5($username) . "<br>";
-echo "Password: " . md5($password) . "<br>";
-echo "<b>✔ md5() done</b><br><br>";
+echo "md5 () done" ."\n";
 
-// SHA512 Section
-echo "<h3 style='color:purple;'>SHA512 Hash</h3>";
-echo "Username: " . hash("SHA512", $username) . "<br>";
-echo "Password: " . hash("SHA512", $password) . "<br>";
-echo "<b>✔ SHA512 hash done</b><br><br>";
+echo hash("SHA512",$username) . "\n";
+echo hash("SHA512",$password) . "\n";
 
-// SHA256 Section
-echo "<h3 style='color:orange;'>SHA256 Hash</h3>";
-echo "Username: " . hash("SHA256", $username) . "<br>";
-echo "Password: " . hash("SHA256", $password) . "<br>";
-echo "<b>✔ SHA256 hash done</b><br>";
+echo ("SHA512 hash") . "\n";
+
+echo hash("SHA256",$username) . "\n";
+echo hash("SHA256",$password) . "\n";
+
+echo ("SHA256 hash") . "\n";
+
+
+
 ?>
